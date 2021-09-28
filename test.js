0fca2896476e9e474282da5ea0a179121b72dd14
nishtha <nishthagoyal8@gmail.com>
2021-09-28 21:58:03 +0530

    hosting

--- test.js
+++
@@ -1,9 +0,0 @@
-let fs = require("fs");
-var xlsx = require('node-xlsx');
-
- var obj = xlsx.parse(__dirname + '/test.csv'); // parses a file
-
-//  var obj = xlsx.parse(fs.readFileSync(__dirname + '/test.xlsx'));
-fs.writeFileSync("data.txt",JSON.stringify(obj))
-
-console.log(obj);
